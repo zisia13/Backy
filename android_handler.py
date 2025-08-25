b73124029ffde518ca11c5b74889996603d97dad
zisia13 <119280919+zisia13@users.noreply.github.com>
2025-08-25 15:30:12 +0200

    progress bar update

--- android_handler.py
+++ android_handler.py
@@ -14,6 +14,8 @@ c_colors = Colors()
 class Android_Handler:
 
     NOT_FOUND = "no_phone_found"
+    PROGRESSBAR_ASCII_COMPLETE = c_colors.GREEN + "-" + c_colors._reset #"█"
+    PROGRESSBAR_ASCII_FINISHED = c_colors.RED + "-" + c_colors._reset
 
     identifier_list = [
         "Android",
@@ -23,6 +25,13 @@ class Android_Handler:
         "S3",
         "S4"
     ]
+
+    DCIM_FOLDER_NAMES = [
+        "Screenshots",
+        "Camera"
+    ]
+
+    PC_SAVE_PATH = r"C:\Users\AGL\Downloads\phone_media_folder_pc"
 
     def __init__(self):
         print(f"This class '{Android_Handler.__name__}' is not intended for an object, please use the classmethods...")
@@ -64,23 +73,15 @@ class Android_Handler:
         return (phone_names, phone_paths)
 
     @classmethod
-    def copy_shell_item(cls, shell_item, destination_path) -> IGNORABLE:
+    def copy_shell_item(cls, phone_file, destination_path) -> IGNORABLE:
         try:
-            os.makedirs(os.path.dirname(destination_path), exist_ok = True)
-
             shell = Dispatch("Shell.Application")
             dest_namespace = shell.NameSpace(os.path.dirname(destination_path))
-
             if not dest_namespace:
-                print(f"❌ Zielordner nicht gefunden: {os.path.dirname(destination_path)}")
                 return False
-            
-            dest_namespace.CopyHere(shell_item)
-
+            dest_namespace.CopyHere(phone_file)
             return True
-
-        except Exception as e:
-            print(f"❌ Kopieren fehlgeschlagen: {e}")
+        except:
             return False
 
     @classmethod
@@ -94,39 +95,41 @@ class Android_Handler:
             return False
 
     @classmethod
-    def get_internal_storage_name(cls, device_name: str) -> IGNORABLE:
+    def get_DCIM_folder_content(cls, device_name: str, wanted_folder_name: str) -> IGNORABLE:
         pythoncom.CoInitialize()
         shell = Dispatch("Shell.Application")
         namespace = shell.NameSpace(17)
 
+        media_list = []
+
         for item in namespace.Items():
             if item.Name == device_name:
                 device_folder = item.GetFolder
-        
                 for storage_item in device_folder.Items():
                     storage_folder = storage_item.GetFolder
-
                     for storage_file in storage_folder.Items():
                         if storage_file.Name == "DCIM":
                             dcim_folder = storage_file.GetFolder
-                        
                             for sub_folder in dcim_folder.Items():
-                                c_colors.c_print(c_colors.GREEN, sub_folder.Name)
-
                                 media_folder = sub_folder.GetFolder
-                                if not str(sub_folder.Name) == "Screenshots":
+                                if sub_folder.Name == wanted_folder_name:
+                                    for media in media_folder.Items():
+                                        if not media.IsFolder: #! folder only
+                                            media_list.append(media)
+                                        else:
+                                            c_colors.c_print(c_colors.YELLOW, f"INFO: Folder: {media.Name} found in {sub_folder.Name}")
+                                else:
                                     continue
+                        else:
+                            continue
+            else:
+                continue
 
-                                for media in media_folder.Items():
-                                    if not media.IsFolder: #! folder only
-                                        print(media.Name)
-                                        c_colors.c_print(c_colors.RED, media.Path)
-                                        pc_path = r"C:\Users\AGL\Downloads\phone_media_folder_pc"
-                                        destination_path = os.path.join(pc_path, media.Name)
+        return media_list    
                                     
-                                        cls.clear_clipboard()
-                                    
-                                        cls.copy_shell_item(media, destination_path)
+    @classmethod
+    def create_media_destination_path(cls, media) -> str:
+        return os.path.join(cls.PC_SAVE_PATH, media.Name)
         
     @classmethod
     def get_every_folder_in_DCIM(cls) -> List[str]:
@@ -137,12 +140,99 @@ class Android_Handler:
         pass
 
     @classmethod
+    def progress_bar(cls, current: str, current_name: str, total: int, width: int, longest_name: int):
+        if len(str(current_name)) < longest_name:
+            diff = longest_name - len(str(current_name))
+            current_name = str(current_name) + str(" " * diff)
+
+        i = total - (total - current)
+        percent = i / total
+        ausgefuellt = int(width * percent)
+        balken = cls.PROGRESSBAR_ASCII_COMPLETE * ausgefuellt + cls.PROGRESSBAR_ASCII_FINISHED * (width - ausgefuellt)
+            
+        sys.stdout.write(f'\r[{balken}] {current}/{total} {percent*100:.1f}% File: {current_name}')
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+    #todo FINISH THIS METHOD
+    @classmethod
+    def end_progress_bar(cls, current: str, current_name: str, total: int, width: int, longest_name: int) -> None:
+        if len(str(current_name)) < longest_name:
+            diff = longest_name - len(str(current_name))
+            current_name = str(current_name) + str(" " * diff)
+
+        i = total - (total - current)
+        percent = i / total
+        ausgefuellt = int(width * percent)
+        balken = '█' * ausgefuellt + '-' * (width - ausgefuellt)
+            
+        sys.stdout.write(f'\r[{balken}] {current}/{total} {percent*100:.1f}% File: {current_name}')
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+    @staticmethod
+    def hide_CLI_cursor():
+        sys.stdout.write("\033[?25l")
+        sys.stdout.flush()
+
+    @staticmethod
+    def show_CLI_cursor():
+        sys.stdout.write("\033[?25h")
+        sys.stdout.flush()
+
+    @classmethod
     def run(cls) -> None:
-        pass
+
+        #! hide cli cursor
+        Android_Handler.hide_CLI_cursor()
+
+        #! init CLI color system
+        os.system("") 
+
+        #! get device name
+        device_names, phone_paths = cls.scan_for_phones()
+        if len(device_names) > 1:
+            c_colors.c_print(c_colors.RED, "ERROR: Multiple Phones found... (select feature will come later)")
+        elif device_names[0] == cls.NOT_FOUND:
+            c_colors.c_print(c_colors.RED, "No Phone Found!")
+        else:
+            selected_phone = device_names[0]
+            print(c_colors.WHITE + "Phone Found: " + c_colors.PURPLE + selected_phone + c_colors._reset)
+
+        #todo cls.clear_clipboard()
+
+        #! get medias in folders
+        all_medias = []
+
+        for folder in cls.DCIM_FOLDER_NAMES:
+            medias = cls.get_DCIM_folder_content(device_name = selected_phone, wanted_folder_name = folder)
+            amount_of_medias = int(len(medias))
+            print(c_colors.WHITE + f"Medias in {folder}: " + c_colors.PURPLE + str(amount_of_medias) + c_colors._reset)
+            for media in medias:
+                all_medias.append(media)
+
+        #! get longest string element of media
+        longest_media_name = 0
+        for media in all_medias:
+            name = media.Name
+            if len(name) > longest_media_name:
+                longest_media_name = len(name)
+
+        #! copy medias
+        media_counter = 1
+        
+        for media in all_medias:
+            media_counter += 1        
+            cls.progress_bar(
+                total = len(all_medias),
+                current = media_counter,
+                current_name = media,
+                width = 30,
+                longest_name = longest_media_name
+            )
+
+        #! show CLI cursor
+        Android_Handler.show_CLI_cursor()
 
 if __name__ == "__main__":
-    ph, pa = Android_Handler.scan_for_phones()
-    print(ph)
-    print(pa)
-
-    Android_Handler.get_internal_storage_name(ph[0])
+    Android_Handler.run()
--- colors.py
+++ colors.py
@@ -31,7 +31,7 @@ class Colors:
     @public
     def c_print(self, c, t):
         print(f"{c}{t}{self._reset}")
-    
 
-    
-    
+    @public
+    def cm_print(self, c1, c2, t1, t2): #todo not working idk why
+        print(f"{c1}{t1}{c2}{t2}{self._reset}")
